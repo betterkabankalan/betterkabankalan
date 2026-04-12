994c43dffed956c6617a8efb8724455eef13d846
reymartlouie <reymartlouie.capapas@icloud.com>
2026-04-12 23:47:19 +0800

    feat: add SEO meta tags, useSEO hook, sitemap, && CRO navigation fixes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,8 @@
-import { Outlet, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout() {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log("Layout sees:", location.pathname, location.search);
-  }, [location.pathname, location.search]);
-
   return (
     <div className="min-h-screen bg-white">
       <Header />
